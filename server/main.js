4fedebfc3b97876c2ec8aa5890686bdf0709f10c
fredhartley <112176646+fredhartley@users.noreply.github.com>
2024-04-29 20:52:36 +0100

    modulised and add playwright

--- server/main.js
+++ server/main.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/chatgpt-request', async (req, res) => {
+app.post("/chatgpt-request", async (req, res) => {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.API_KEY}`,
-          'Content-Type': 'application/json'
+            Authorization: `Bearer ${process.env.API_KEY}`,
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: [{
-                role: "user",
-                content: req.body.message,
-            }]
-        })
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: req.body.message,
+                },
+            ],
+        }),
     });
 
     const json = await response.json();
 
     res.send({ choices: json.choices, created: json.created });
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
