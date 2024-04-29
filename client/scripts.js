c2c8013fd3c8a8dff1e64d25d528eefe784323db
fredhartley <112176646+fredhartley@users.noreply.github.com>
2024-04-29 19:44:38 +0100

    Added nodemon

--- client/scripts.js
+++ client/scripts.js
@@ -1,99 +1,95 @@
-
-
-
 const queryForm = document.getElementById("chatgpt-query");
 const promptInput = document.getElementById("prompt-input");
 const promptHistory = document.getElementById("prompt-history");
 
-
 const displayUserInput = (userInputText) => {
-    const userInputContainer = document.createElement("div");
-    userInputContainer.classList.add("user-input__container")
+  const userInputContainer = document.createElement("div");
+  userInputContainer.classList.add("user-input__container");
 
-    const userInputTitle = document.createElement("h3");
-    userInputTitle.appendChild(document.createTextNode("You"));
+  const userInputTitle = document.createElement("h3");
+  userInputTitle.appendChild(document.createTextNode("You"));
 
-    const userInput = document.createElement("p");
-    userInput.appendChild(document.createTextNode(userInputText));
+  const userInput = document.createElement("p");
+  userInput.appendChild(document.createTextNode(userInputText));
 
-    userInputContainer.append(userInputTitle, userInput);
+  userInputContainer.append(userInputTitle, userInput);
 
-    return userInputContainer
-}
+  return userInputContainer;
+};
 
 const displayChatgptResponse = (promptHistory, timestamp) => {
-    const responseContainer = document.createElement("div");
-    responseContainer.classList.add("response__container");
+  const responseContainer = document.createElement("div");
+  responseContainer.classList.add("response__container");
 
-    const responseTitle = document.createElement("h3");
-    responseTitle.appendChild(document.createTextNode("ChatGPT"));
+  const responseTitle = document.createElement("h3");
+  responseTitle.appendChild(document.createTextNode("ChatGPT"));
 
-    const responseInput = document.createElement("p");
-    responseInput.appendChild(document.createTextNode(promptHistory));
-    responseContainer.append(responseTitle, responseInput);
-    
-    const date = new Date(timestamp * 1000);
-    const dateElem = document.createTextNode(`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`)
-    
-    const timestampElem = document.createElement("time");
-    timestampElem.classList.add("timestamp");
-    timestampElem.appendChild(dateElem);
+  const responseInput = document.createElement("p");
+  responseInput.appendChild(document.createTextNode(promptHistory));
+  responseContainer.append(responseTitle, responseInput);
 
-    responseContainer.appendChild(timestampElem);
+  const date = new Date(timestamp * 1000);
+  const dateElem = document.createTextNode(
+    `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+  );
 
-    // container.append(userInputContainer, responseContainer, timestampElem);
+  const timestampElem = document.createElement("time");
+  timestampElem.classList.add("timestamp");
+  timestampElem.appendChild(dateElem);
 
-    return responseContainer;
-}   
+  responseContainer.appendChild(timestampElem);
 
+  // container.append(userInputContainer, responseContainer, timestampElem);
+
+  return responseContainer;
+};
 
 const onRequestComplete = async (req, container) => {
-    const json = await req.json();
-    const choices = json.choices;
-    const timestamp = json.created;
+  const json = await req.json();
+  const choices = json.choices;
+  const timestamp = json.created;
 
-    container.appendChild(displayChatgptResponse(choices[0].message.content, timestamp));
-}
+  container.appendChild(
+    displayChatgptResponse(choices[0].message.content, timestamp)
+  );
+};
 
 const onRequestFaied = async (error) => {
-    promptHistory.textContent = `There has been an error: ${error}`;
-}
+  promptHistory.textContent = `There has been an error: ${error}`;
+};
 
 const onFormSubmit = () => {
-    const userInput = promptInput.value;
+  const userInput = promptInput.value;
 
-    if (userInput == "") {
-        return
-    }
+  if (userInput == "") {
+    return;
+  }
 
-    promptInput.value = "";
+  promptInput.value = "";
 
-    const container = document.createElement("div");
-    container.appendChild(displayUserInput(userInput));
-    promptHistory.appendChild(container);
+  const container = document.createElement("div");
+  container.appendChild(displayUserInput(userInput));
+  promptHistory.appendChild(container);
 
-    fetch('http://127.0.0.1:3000/chatgpt-request', 
-    { 
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({message: userInput}) 
-    })
+  fetch("http://127.0.0.1:3000/chatgpt-request", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: userInput }),
+  })
     .then((req) => {
-        onRequestComplete(req, container)
+      onRequestComplete(req, container);
     })
-    .catch(onRequestFaied)
-}
+    .catch(onRequestFaied);
+};
 
 queryForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    onFormSubmit()
+  e.preventDefault();
+  onFormSubmit();
 });
 
 promptInput.addEventListener("keydown", (e) => {
-    if(e.key == "Enter" && !e.shiftKey) {
-        e.preventDefault()
-        onFormSubmit()
-    }
-})
-
-
+  if (e.key == "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    onFormSubmit();
+  }
+});
