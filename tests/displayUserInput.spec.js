aabe10c9d267fcb7dcba1c24baaf7e7b58d68998
fredhartley <112176646+fredhartley@users.noreply.github.com>
2024-04-30 08:43:43 +0100

    Added input and output test

--- tests/displayUserInput.spec.js
+++ tests/displayUserInput.spec.js
@@ -1,5 +1,17 @@
 const { test, expect } = require("@playwright/test");
 
-test.beforeEach(async ({ page }) => {
+test("testing form input and output", async ({ page }) => {
     await page.goto("http://127.0.0.1:5500/client/index.html");
+
+    const userInput = "This is test text.";
+
+    await page.locator("#prompt-input").fill(userInput);
+
+    await page.locator("#submit-button").click();
+
+    const textOutput = await page
+        .locator(".user-input__container p")
+        .textContent();
+
+    expect(textOutput).toBe(userInput);
 });
